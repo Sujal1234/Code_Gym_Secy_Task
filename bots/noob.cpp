80b9c6b1bd355b0a6e62796244128f25443cc05e
Sujal1234 <sujal.motagi@gmail.com>
2025-05-24 20:45:02 +0000

    Added a basic bot

---
+++ bots/noob.cpp
@@ -0,0 +1,97 @@
+#include <iostream>
+#include <string>
+#include <random>
+#include <array>
+#include <cstddef>
+
+constexpr int GRID_SIZE = 20;
+
+bool isValidPosition(int x, int y){
+    return x >= 0 && x < GRID_SIZE && y >= 0 && y < GRID_SIZE;
+}
+
+int main(){
+    int x, y, ignore;
+    std::cin >> x >> y;
+    for (int i = 0; i < 6; i++)
+    {
+        int ignore;
+        std::cin >> ignore;
+    }
+    std::array<std::array<char, GRID_SIZE>, GRID_SIZE> grid;
+    for(int i = 0; i < GRID_SIZE; ++i){
+        for(int j = 0; j < GRID_SIZE; ++j){
+            std::cin >> grid[i][j];
+        }
+    }
+
+    std::string dirs[4] = {"UP", "DOWN", "LEFT", "RIGHT"};
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    
+    std::uniform_int_distribution<std::size_t> dis(0, 3);
+
+    std::size_t ind = dis(gen);
+    int newX {x}, newY {y};
+    switch(ind){
+        case 0: newY--; break; //UP
+        case 1: newY++; break; //DOWN
+        case 2: newX--; break; //LEFT
+        case 3: newX++; break; //RIGHT
+    }
+    while(!isValidPosition(newX, newY) || grid[newY][newX] != '.'){
+        ind = dis(gen);
+        newX = x;
+        newY = y;
+        switch(ind){
+            case 0: newY--; break; //UP
+            case 1: newY++; break; //DOWN
+            case 2: newX--; break; //LEFT
+            case 3: newX++; break; //RIGHT
+        }
+    }
+
+    std::cerr << "NOOB: " << "MOVE " << dirs[ind] << " BOMB -1 -1 ATTACK -1 -1" << std::endl;
+    std::cout << "MOVE " << dirs[ind] << " BOMB -1 -1 ATTACK -1 -1" << std::endl;
+
+    while(true){
+        std::string oppDir;
+        std::cin >> oppDir >> oppDir; //Ignore the first input ("MOVE")
+        
+        std::cin >> x >> y;
+        for (int i = 0; i < 6; i++)
+        {
+            std::cin >> ignore;
+        }
+
+        for(int i = 0; i < GRID_SIZE; ++i){
+            for(int j = 0; j < GRID_SIZE; ++j){
+                std::cin >> grid[i][j];
+            }
+        }
+        
+        ind = dis(gen);
+        newX = x;
+        newY = y;
+        switch(ind){
+            case 0: newY--; break; //UP
+            case 1: newY++; break; //DOWN
+            case 2: newX--; break; //LEFT
+            case 3: newX++; break; //RIGHT
+        }
+        while(!isValidPosition(newX, newY) || grid[newY][newX] != '.'){
+            ind = dis(gen);
+            newX = x;
+            newY = y;
+            switch(ind){
+                case 0: newY--; break; //UP
+                case 1: newY++; break; //DOWN
+                case 2: newX--; break; //LEFT
+                case 3: newX++; break; //RIGHT
+            }
+        }
+        
+        std::cerr << "NOOB: " << "MOVE " << dirs[ind] << " BOMB -1 -1 ATTACK -1 -1" << std::endl;
+        std::cout << "MOVE " << dirs[ind] << " BOMB -1 -1 ATTACK -1 -1" << std::endl;
+    }
+}
